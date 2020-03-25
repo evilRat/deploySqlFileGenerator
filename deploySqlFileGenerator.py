9e01fabe9db052c4ca1bf8ccb8afaeba167faeef
Kong Zheng <kongzheng1993@hotmail.com>
2020-03-25 18:15:50 +0800

    完成主要工作，回退和备份脚本待完成

--- deploySqlFileGenerator.py
+++ deploySqlFileGenerator.py
@@ -12,19 +12,25 @@ def handleSqls(file):
     update = []
     delete = []
     insert = []
+    alter = []
+    index = []
     for sql in sqls:
         logger.info('当前处理的sql：' + sql)
-        if sql.strip().split(' ')[0].lower() == 'create':
+        if sql.split(' ')[0].lower().strip() == 'create':
             create.append(sql)
-        elif sql.strip().split(' ')[0].lower() == 'update':
-            update.append(sql)
-        elif sql.strip().split(' ')[0].lower() == 'delete':
-            delete.append(sql)
-        elif sql.strip().split(' ')[0].lower() == 'insert':
+        elif sql.split(' ')[0].lower().strip() == 'insert':
             insert.append(sql)
+        elif sql.split(' ')[0].lower().strip() == 'update':
+            update.append(sql)
+        elif sql.split(' ')[0].lower().strip() == 'delete':
+            delete.append(sql)
+        elif sql.split(' ')[0].lower().strip() == 'alter':
+            alter.append(sql)
+        elif sql.split(' ')[0].lower().strip() == 'index':
+            index.append(sql)
         else:
             logger.error("sql类型没有匹配到：" + sql)
-    return {"create": create, "update": update, "delete": delete, "insert": insert}
+    return {"create": create, "update": update, "delete": delete, "insert": insert, "alter": alter, "index": index}
 
 
 
@@ -42,49 +48,16 @@ if __name__=="__main__":
     logger.addHandler(handler)
     #banner
     banner = '''
-                                                                                                                           
-                                                                                                                       
-KKKKKKKKK    KKKKKKK                                                                                                   
-K:::::::K    K:::::K                                                                                                   
-K:::::::K    K:::::K                                                                                                   
-K:::::::K   K::::::K                                                                                                   
-KK::::::K  K:::::KKK   ooooooooooo   nnnn  nnnnnnnn       ggggggggg   gggggzzzzzzzzzzzzzzzzz                           
-  K:::::K K:::::K    oo:::::::::::oo n:::nn::::::::nn    g:::::::::ggg::::gz:::::::::::::::z                           
-  K::::::K:::::K    o:::::::::::::::on::::::::::::::nn  g:::::::::::::::::gz::::::::::::::z                            
-  K:::::::::::K     o:::::ooooo:::::onn:::::::::::::::ng::::::ggggg::::::ggzzzzzzzz::::::z                             
-  K:::::::::::K     o::::o     o::::o  n:::::nnnn:::::ng:::::g     g:::::g       z::::::z                              
-  K::::::K:::::K    o::::o     o::::o  n::::n    n::::ng:::::g     g:::::g      z::::::z                               
-  K:::::K K:::::K   o::::o     o::::o  n::::n    n::::ng:::::g     g:::::g     z::::::z                                
-KK::::::K  K:::::KKKo::::o     o::::o  n::::n    n::::ng::::::g    g:::::g    z::::::z                                 
-K:::::::K   K::::::Ko:::::ooooo:::::o  n::::n    n::::ng:::::::ggggg:::::g   z::::::zzzzzzzz                           
-K:::::::K    K:::::Ko:::::::::::::::o  n::::n    n::::n g::::::::::::::::g  z::::::::::::::z                           
-K:::::::K    K:::::K oo:::::::::::oo   n::::n    n::::n  gg::::::::::::::g z:::::::::::::::z                           
-KKKKKKKKK    KKKKKKK   ooooooooooo     nnnnnn    nnnnnn    gggggggg::::::g zzzzzzzzzzzzzzzzz                           
-                                                                   g:::::g                                             
-                                                       gggggg      g:::::g                                             
-                                                       g:::::gg   gg:::::g                                             
-                                                        g::::::ggg:::::::g                                             
-                                                         gg:::::::::::::g                                              
-                                                           ggg::::::ggg                                                
-                                                              gggggg                                                   
-                                                                                                                       
-                                                                                                                       
-                                             iiii  lllllll RRRRRRRRRRRRRRRRR                             tttt          
-                                            i::::i l:::::l R::::::::::::::::R                         ttt:::t          
-                                             iiii  l:::::l R::::::RRRRRR:::::R                        t:::::t          
-                                                   l:::::l RR:::::R     R:::::R                       t:::::t          
-    eeeeeeeeeeee  vvvvvvv           vvvvvvviiiiiii  l::::l   R::::R     R:::::R  aaaaaaaaaaaaa  ttttttt:::::ttttttt    
-  ee::::::::::::ee v:::::v         v:::::v i:::::i  l::::l   R::::R     R:::::R  a::::::::::::a t:::::::::::::::::t    
- e::::::eeeee:::::eev:::::v       v:::::v   i::::i  l::::l   R::::RRRRRR:::::R   aaaaaaaaa:::::at:::::::::::::::::t    
-e::::::e     e:::::e v:::::v     v:::::v    i::::i  l::::l   R:::::::::::::RR             a::::atttttt:::::::tttttt    
-e:::::::eeeee::::::e  v:::::v   v:::::v     i::::i  l::::l   R::::RRRRRR:::::R     aaaaaaa:::::a      t:::::t          
-e:::::::::::::::::e    v:::::v v:::::v      i::::i  l::::l   R::::R     R:::::R  aa::::::::::::a      t:::::t          
-e::::::eeeeeeeeeee      v:::::v:::::v       i::::i  l::::l   R::::R     R:::::R a::::aaaa::::::a      t:::::t          
-e:::::::e                v:::::::::v        i::::i  l::::l   R::::R     R:::::Ra::::a    a:::::a      t:::::t    tttttt
-e::::::::e                v:::::::v        i::::::il::::::lRR:::::R     R:::::Ra::::a    a:::::a      t::::::tttt:::::t
- e::::::::eeeeeeee         v:::::v         i::::::il::::::lR::::::R     R:::::Ra:::::aaaa::::::a      tt::::::::::::::t
-  ee:::::::::::::e          v:::v          i::::::il::::::lR::::::R     R:::::R a::::::::::aa:::a       tt:::::::::::tt
-    eeeeeeeeeeeeee           vvv           iiiiiiiillllllllRRRRRRRR     RRRRRRR  aaaaaaaaaa  aaaa         ttttttttttt  
+        __                          
+   / /______  ____  ____ _____  
+  / //_/ __ \/ __ \/ __ `/_  /  
+ / ,< / /_/ / / / / /_/ / / /_  
+/_/|_|\____/_/ /_/\__, / /___/  
+             _ __////_/      __ 
+  ___ _   __(_) / __ \____ _/ /_
+ / _ \ | / / / / /_/ / __ `/ __/
+/  __/ |/ / / / _, _/ /_/ / /_  
+\___/|___/_/_/_/ |_|\__,_/\__/    
                                                                                                                        '''
     logger.info(banner)
     logger.info("=====================上线SQL脚本文件生成工具=====================")
@@ -103,5 +76,42 @@ e::::::::e                v:::::::v        i::::::il::::::lRR:::::R     R:::::Ra
     logger.info("sqlDict:")
     print(sqlDict)
 
-    print("fuck")
+    #文件名
+    fileName = '亦庄-北京接口平台-生产-nglocal_bj-%(deployDate)s-%(fileNum)d-%(fileNo)d-%(sqlType)s-sql.txt'
 
+    #文件头
+    fileHeader = '''/*
+*地址：198.115.168.53
+*端口号：20001
+*库名：nglocal_bj
+*用户名：nglocal_bj
+*操作数量：%(sqlNum)d
+*是否中间件：否
+*数据库类型：mysql
+*工单联系人电话: 17610119557
+*工单执行失败是否回滚:部分回滚
+*/'''
+
+    #文件序号
+    fileNo=0
+
+    #删除没有的sql类型
+    for key in list(sqlDict.keys()):
+        if sqlDict[key] == []:
+            sqlDict.pop(key)
+
+    #分类输出
+    for sqlType in sqlDict:
+        fileNo=fileNo + 1
+        f = open((fileName % dict(deployDate=deployDate, fileNum=len(sqlDict), fileNo=fileNo, sqlType=sqlType)), 'w+', encoding='utf-8')
+        f.write(fileHeader % dict (sqlNum=len(sqlDict[sqlType])))
+        f.write('\n')
+        for sql in sqlDict[sqlType]:
+            f.write(sql)
+            f.write(';')
+        f.close()
+        logger.info(sqlType + '类型的sql脚本文件已经生成。')
+
+    logger.info("finish")
+
+    input("press enter to exit...")
